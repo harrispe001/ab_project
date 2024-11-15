3bdb361c9acc6ca6cc4a1bc4622c755bdcddfd5e
harrispe001 <114611473+harrispe001@users.noreply.github.com>
2024-11-15 10:18:41 -0600

    uses AWS secrets manager to get credentials

--- Program.cs
+++ Program.cs
@@ -3,16 +3,59 @@ using ab_project.Data;
 using ab_project.Models;
 using ab_project.Repositories;
 using ab_project.Services;
+using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Function to get secret
+string GetSecret()
+{
+    string secretName = "ab_project/dbcredentials";
+    string region = "us-east-1";
+
+    IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+
+    GetSecretValueRequest request = new GetSecretValueRequest
+    {
+        SecretId = secretName,
+        VersionStage = "AWSCURRENT",
+    };
+
+    try
+    {
+        var response = client.GetSecretValueAsync(request).Result;
+        return response.SecretString;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Error retrieving secret: {e.Message}");
+        throw;
+    }
+}
+
+// Retrieve the secret
+var secretJson = GetSecret();
+
+// Parse the JSON string to get the database credentials
+var dbCredentials = JsonSerializer.Deserialize<Dictionary<string, string>>(secretJson);
+
+// Construct the connection string
+var connectionString = $"server={dbCredentials["host"]};" +
+                       $"port={dbCredentials["port"]};" +
+                       $"database={dbCredentials["dbname"]};" +
+                       $"user={dbCredentials["username"]};" +
+                       $"password={dbCredentials["password"]}";
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
     ));
 
 builder.Services.AddScoped<ITrainingRepository, TrainingRepository>();
--- obj/Debug/net8.0/ab_project.AssemblyInfo.cs
+++ obj/Debug/net8.0/ab_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ab_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a641cd50b3c8b9759bf0a23cdc0c941da310ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("ab_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ab_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
